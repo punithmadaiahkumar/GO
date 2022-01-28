5492b33971e321d2896f04d8068cc8050fe7ead7
punithmadaiahkumar <punithmadaiahkumar@gmail.com>
2022-01-28 10:54:13 +0530

    updated main.go file frm package scope

--- 03_Scope/01_Package-Scope/main/main.go
+++ 03_Scope/01_Package-Scope/main/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/punithmadaiahkumar/GO/tree/master/03_Scope/01_Package-Scope/vis"
+	"github.com/punithmadaiahkumar/GO/03_Scope/01_Package-Scope/vis"
 )
 
 func main() {
