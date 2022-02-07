6301daae951130cb32081d34c877dd7f9d02109f
punithmadaiahkumar <punithmadaiahkumar@gmail.com>
2022-02-07 12:42:10 +0530

    ceated if.go script

---
+++ 03_Branching/if.go
@@ -0,0 +1,12 @@
+package main
+
+import "./greeting";
+
+func main() {
+
+	var github = greeting.Profile{"Punith Gowda", "PunithGowda", "Welcome to Go world!"}
+
+	/**
+	 * Call the function and pass the data to the function
+	 */
+	greeting.Greeting(github, greeting.CreatePrintFunction("?"), false)
